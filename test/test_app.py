5fbcf115caa9f0b151a20d8fbc9f89808312bd99
aymnotime <aymen.atif10@gmail.com>
2025-04-06 21:21:44 +0200

    Initialisation du projet CI/CD

---
+++ test/test_app.py
@@ -0,0 +1,7 @@
+import pytest
+import requests
+
+def test_home():
+    response = requests.get("http://127.0.0.1:5000/")
+    assert response.status_code == 200
+    assert response.text == "Hello world, CI/CD!"
