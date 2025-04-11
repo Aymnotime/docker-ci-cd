dde43bf5b6a5c39ce2188e85cdebcaed5aeb2219
aymnotime <aymen.atif10@gmail.com>
2025-04-11 19:26:34 +0200

    modif test_app

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,8 +2,7 @@ import os
 import sys
 import pytest
 
-# Ajouter dynamiquement le chemin du projet au PYTHONPATH
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from app import app
 
